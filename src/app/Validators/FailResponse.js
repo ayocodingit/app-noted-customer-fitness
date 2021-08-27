e6411792a681eff5c8d11af4fc6c58d7d8b5cd21
ayocodingit <ayocodingit@gmail.com>
2021-08-28 06:37:55 +0700

    sync boilerplate

--- src/app/Validators/FailResponse.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const { StatusCodes } = require('http-status-codes')
-
-module.exports = (ctx, errorMessages) => {
-  return ctx.response
-    .status(StatusCodes.UNPROCESSABLE_ENTITY)
-    .json({ errors: errorMessages })
-}
